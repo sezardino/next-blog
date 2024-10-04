a2826923d35e4eee14531276ea73b9f4491af014
Edward <e.arechwa@gmail.com>
2024-10-04 11:10:15 +0200

    ft: add seed posts

--- src/app/(app)/post/actions/seed-posts.ts
+++ src/app/(app)/post/actions/seed-posts.ts
@@ -1,59 +1,67 @@
 "use server";
 
-import { ProjectUrls } from "@/const";
 import { defaultPostsForSeed } from "@/const/seeds";
 import prismaClient from "@/lib/prisma";
 import { normalizeTags } from "@/utils/post";
-
 import { auth } from "@clerk/nextjs/server";
 
-import { faker } from "@faker-js/faker";
-import { revalidatePath } from "next/cache";
+import dayjs from "dayjs";
 
-const generateFakePosts = (length: number) => {
-  return Array.from({ length }, () => {
-    const status = faker.helpers.arrayElement([
-      "PUBLISHED",
-      "SCHEDULED",
-      "DRAFT",
-    ]);
-    const publicationDate =
-      status === "PUBLISHED"
-        ? faker.date.past()
-        : status === "SCHEDULED"
-        ? faker.date.future()
-        : null;
+function generatePublicationDate(index: number): Date {
+  if (index === 0) {
+    return dayjs().startOf("day").toDate();
+  }
 
-    const tagsCount = faker.number.int({ min: 1, max: 10 });
-
-    const tags = Array.from({ length: tagsCount }, () =>
-      faker.word.adjective()
-    );
-
-    return {
-      title: faker.lorem.sentence(),
-      description: faker.lorem.sentence(),
-      body: faker.lorem.paragraphs(3),
-      tags,
-      publicationDate,
-      isPublished: status === "PUBLISHED",
-    };
-  });
-};
+  return dayjs()
+    .add(index * 5, "day")
+    .startOf("day")
+    .toDate();
+}
 
 export const seedPostsForCurrentUser = async () => {
   const { userId } = auth();
+  if (!userId) return { message: "User not provided" };
 
-  if (!userId) throw new Error("Unauthorized");
-
-  const posts = defaultPostsForSeed;
-  const allTags = Array.from(
-    new Set(normalizeTags(posts.map((p) => p.tags).flat()))
-  );
+  const posts = defaultPostsForSeed.map((post, index) => ({
+    ...post,
+    publicationDate: generatePublicationDate(index),
+    isPublished: index === 0,
+  }));
 
   try {
+    const existingPosts = await prismaClient.post.findMany({
+      where: {
+        author: { clerkId: userId },
+        title: { in: posts.map((post) => post.title) },
+      },
+      select: { title: true },
+    });
+
+    // check what post already exist
+    const existingTitles = existingPosts.map((post) => post.title);
+    const newPosts = posts.filter(
+      (post) => !existingTitles.includes(post.title)
+    );
+
+    if (newPosts.length === 0) {
+      console.log("No new posts to create, all posts already exist.");
+      return { message: "All posts already exist for the current user." };
+    }
+
+    const allTags = Array.from(
+      new Set(normalizeTags(newPosts.map((p) => p.tags).flat()))
+    );
+
+    const existingTags = await prismaClient.tag.findMany({
+      where: { name: { in: allTags } },
+      select: { name: true },
+    });
+
+    const existingTagNames = existingTags.map((tag) => tag.name);
+    const newTags = allTags.filter((tag) => !existingTagNames.includes(tag));
+
     await Promise.all([
-      ...allTags.map((name) => prismaClient.tag.create({ data: { name } })),
+      ...newTags.map((name) => prismaClient.tag.create({ data: { name } })),
       ...posts.map(({ tags, ...post }) =>
         prismaClient.post.create({
           data: {
@@ -66,8 +74,6 @@ export const seedPostsForCurrentUser = async () => {
         })
       ),
     ]);
-
-    revalidatePath(ProjectUrls.myPosts);
   } catch (error) {
     console.error("Error creating seeds:", error);
     return { message: "There was an error when trying to seed posts" };
